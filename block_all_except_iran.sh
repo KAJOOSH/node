0b9276e6c9a5be1bb257b688d799ebe4187b8940
Sajjad Emami <47478490+KAJOOSH@users.noreply.github.com>
2024-12-09 11:53:08 +0330

    Update block_all_except_iran.sh

--- block_all_except_iran.sh
+++ block_all_except_iran.sh
@@ -18,8 +18,8 @@ ip6tables -A INPUT -i lo -j ACCEPT
 
 iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 ip6tables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A INPUT -s 91.107.178.21 -j ACCEPT
 
-# اعمال قوانین برای IPv4
 if [ -f "$V4_FILE" ]; then
     echo "Applying IPv4 rules..."
     for IP in $(cat "$V4_FILE"); do
@@ -29,7 +29,6 @@ else
     echo "$V4_FILE not found!"
 fi
 
-# اعمال قوانین برای IPv6
 if [ -f "$V6_FILE" ]; then
     echo "Applying IPv6 rules..."
     for IP in $(cat "$V6_FILE"); do
@@ -41,7 +40,5 @@ fi
 
 iptables-save > /etc/iptables/rules.v4
 ip6tables-save > /etc/iptables/rules.v6
-
-iptables -A INPUT -s 91.107.178.21 -j ACCEPT
 
 echo "Rules applied successfully for both IPv4 and IPv6."

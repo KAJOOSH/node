cdce92f1a3315e1690441c245f9ab5e5311a5735
Sajjad Emami <47478490+KAJOOSH@users.noreply.github.com>
2024-12-09 10:24:51 +0330

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -58,6 +58,7 @@ sudo echo -e $'\e[32mMarzban Node is Up and Running successfully.\e[0m'
 DEST_DIR="/usr/local/block-all-except-iran"
 
 mkdir -p "$DEST_DIR"
+cd "$DEST_DIR"
 
 FETCH_IPV4_SCRIPT_URL="https://raw.githubusercontent.com/KAJOOSH/node/refs/heads/main/fetch_iran_ips_v4.sh"
 FETCH_IPV6_SCRIPT_URL="https://raw.githubusercontent.com/KAJOOSH/node/refs/heads/main/fetch_iran_ips_v6.sh"

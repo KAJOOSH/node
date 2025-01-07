101ded96df980c40da4888b3f6513c74241103d2
Sajjad Emami <47478490+KAJOOSH@users.noreply.github.com>
2025-01-07 18:12:07 +0330

    Create xray-update.sh

---
+++ xray-update.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+echo "Please enter the tag (e.g., v24.12.31):"
+read tag
+
+if [ -z "$tag" ]; then
+    echo "Error: Tag cannot be empty. Please run the script again and provide a valid tag."
+    exit 1
+fi
+
+echo "Please select the operating system:"
+echo "1) linux-64"
+echo "2) linux-arm64"
+read -p "Enter the number corresponding to your choice: " os_choice
+
+case $os_choice in
+    1)
+        os="linux-64"
+        url="https://github.com/XTLS/Xray-core/releases/download/$tag/Xray-linux-64.zip"
+        filename="Xray-linux-64.zip"
+        ;;
+    2)
+        os="linux-arm64"
+        url="https://github.com/XTLS/Xray-core/releases/download/$tag/Xray-linux-arm64-v8a.zip"
+        filename="Xray-linux-arm64-v8a.zip"
+        ;;
+    *)
+        echo "Error: Invalid choice. Please run the script again and select a valid option."
+        exit 1
+        ;;
+esac
+
+echo "Tag: $tag"
+echo "Operating System: $os"
+echo "Downloading from: $url"
+
+sudo wget "$url"
+
+sudo unzip -o "$filename"
+
+sudo rm "$filename"
+
+cd ~/Marzban-node
+
+sudo docker compose down && sudo docker compose up -d
+
+sudo echo -e $'\e[32mMarzban Node is Up and Running successfully.\e[0m'
